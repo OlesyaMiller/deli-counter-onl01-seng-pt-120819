3e8f21f44a1a667aa2029387c8a705ee2780cd07
Olesya Miller <olesyam.miller@gmail.com>
2019-12-11 16:00:58 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,21 @@
-# Write your code here.
+require 'pry'
+
+def line(new_line)
+  
+  katz_deli = []
+ 
+  if new_line.length > 0 
+    new_line.each_with_index { |person, index| katz_deli << "#{index + 1}. #{person}" }
+    puts "The line is currently: #{katz_deli.join(" ")}"
+  else
+    puts "The line is currently empty."
+  end
+end 
+
+
+def take_a_number(current_line, name)
+  
+end
+  
+  
+  
