e305e9ce3b076c1303917696783583670b08763b
Olesya Miller <olesyam.miller@gmail.com>
2019-12-11 16:59:16 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,14 +2,14 @@ require 'pry'
 
 def line(new_line)
   
-  katz_deli = []
+  #katz_deli = []
  
-  if new_line.length > 0 
-    new_line.each_with_index { |person, index| katz_deli << "#{index + 1}. #{person}" }
-    puts "The line is currently: #{katz_deli.join(" ")}"
-  else
-    puts "The line is currently empty."
-  end
+  # if new_line.length > 0 
+  #   new_line.each_with_index { |person, index| katz_deli << "#{index + 1}. #{person}" }
+  #   puts "The line is currently: #{katz_deli.join(" ")}"
+  # else
+  #   puts "The line is currently empty."
+  # end
 end 
 
 
